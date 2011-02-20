0d6338ae349680b12dd93bc40bdc5ebd8333d1b7
lexi@crowdleaks.org <lexi@crowdleaks.org>
2011-02-20 23:54:48 +0000

    Removed single email viewer

--- server.php
+++ server.php
@@ -45,91 +45,53 @@ function GetSQL($count)
 	return $sql;
 }
 
-function GetSingle()
-{
-	$database = GetDatabase();
-	$sql = "SELECT htmlemails.Body, emails.Subject, emails.SentFrom, emails.SentTo, emails.EmailDate, emails.Filename from htmlemails inner join emails on htmlemails.Id = emails.Id WHERE emails.Id = " . intval($_GET['id']) . " LIMIT 1" ;
-	$result = mysql_query($sql, $database);
-	return mysql_fetch_assoc($result);
-}
-
-$id = intval($_REQUEST['id']);
 $db = GetDatabase();
-if(isset($_REQUEST['id']) && $id > 0)
-{
-	$row = GetSingle();
-	
-	?>
-		<div>
-			Subject: <?php echo $row["Subject"];?>
-		</div>
-		<div>
-		Sent From:<?php echo $row["SentFrom"];?>
-		</div>
-		<div>
-		Sent To:<?php echo $row["SentTo"];?>
-		</div>
-		<div>
-		Date:<?php echo $row["EmailDate"];?>
-		</div>
-		<div>
-		Filename:<a href="<?php echo $row["Filename"];?>">Original File</a>
-		</div>
-		<hr/>
-		<div>
-			<?php echo $row["Body"];?>
-		</div>
-	<?php
-	
+
+$page = $_REQUEST['page']; // get the requested page
+$limit = $_REQUEST['rows']; // get how many rows we want to have into the grid
+ // get index row - i.e. user click to sort
+$sord = $_REQUEST['sord'] == "asc"?"asc":"desc"; // get the direction
+
+$columns = array('Subject','SentFrom','SentTo','EmailDate');
+if (in_array($_GET['sort_column'], $columns)) {
+	$sidx = $_REQUEST['sidx'];
+} else {
+	$sidx = $columns[0];
 }
-else
-{
-	$page = $_REQUEST['page']; // get the requested page
-	$limit = $_REQUEST['rows']; // get how many rows we want to have into the grid
-	 // get index row - i.e. user click to sort
-	$sord = $_REQUEST['sord'] == "asc"?"asc":"desc"; // get the direction
-	
-	$columns = array('Subject','SentFrom','SentTo','EmailDate');
-	if (in_array($_GET['sort_column'], $columns)) {
-		$sidx = $_REQUEST['sidx'];
-	} else {
-		$sidx = $columns[0];
-	}
 
-	$totalrows = isset($_REQUEST['totalrows']) ? $_REQUEST['totalrows']: false;
-	if($totalrows) {
-		$limit = $totalrows;
-	}
-
-	if(!$sidx) $sidx =1;
-	// connect to the database
-	$db = GetDatabase();
-
-	$result = mysql_query(GetSQL(true),$db);
-	$row = mysql_fetch_array($result,MYSQL_ASSOC);
-	$count = $row['count'];
-
-	if( $count >0 ) {
-		$total_pages = ceil($count/$limit);
-	} else {
-		$total_pages = 0;
-	}
-	if ($page > $total_pages) $page=$total_pages;	
-	$start = $limit*$page - $limit; // do not put $limit*($page - 1)
-	if ($start<0) $start = 0;
-	$SQL = GetSQL(false) . " ORDER BY ".$sidx." ".$sord. " LIMIT ".(int)$start." , ".(int)$limit;
-	$result = mysql_query( $SQL, $db );
-	$responce->page = $page;
-	$responce->total = $total_pages;
-	$responce->records = $count;
-	$i=0;
-	while($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
-	$responce->rows[$i]['Id']=$row[Id];
-		$responce->rows[$i]['cell']=array($row[Id],htmlspecialchars($row[Subject]),htmlspecialchars($row[SentFrom]),htmlspecialchars($row[SentTo]), $row[EmailDate], $row[link]);
-		$i++;
-	}
-	echo json_encode($responce);
+$totalrows = isset($_REQUEST['totalrows']) ? $_REQUEST['totalrows']: false;
+if($totalrows) {
+	$limit = $totalrows;
 }
+
+if(!$sidx) $sidx =1;
+// connect to the database
+$db = GetDatabase();
+
+$result = mysql_query(GetSQL(true),$db);
+$row = mysql_fetch_array($result,MYSQL_ASSOC);
+$count = $row['count'];
+
+if( $count >0 ) {
+	$total_pages = ceil($count/$limit);
+} else {
+	$total_pages = 0;
+}
+if ($page > $total_pages) $page=$total_pages;	
+$start = $limit*$page - $limit; // do not put $limit*($page - 1)
+if ($start<0) $start = 0;
+$SQL = GetSQL(false) . " ORDER BY ".$sidx." ".$sord. " LIMIT ".(int)$start." , ".(int)$limit;
+$result = mysql_query( $SQL, $db );
+$responce->page = $page;
+$responce->total = $total_pages;
+$responce->records = $count;
+$i=0;
+while($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
+$responce->rows[$i]['Id']=$row[Id];
+	$responce->rows[$i]['cell']=array($row[Id],htmlspecialchars($row[Subject]),htmlspecialchars($row[SentFrom]),htmlspecialchars($row[SentTo]), $row[EmailDate], $row[link]);
+	$i++;
+}
+echo json_encode($responce);
 mysql_close($db);
 
 
